fe4f51d632ba0a6d075acf8d953648dc8bad5e4f
Anaeze <nzediegwu1@gmail.com>
2020-09-07 22:38:39 +0100

    attempt to implement search

--- src/App.js
+++ src/App.js
@@ -1,59 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Row, Container } from 'react-bootstrap';
 import { HomeCard, NoResource, VisionNavBar, ErrorToast } from './components';
 import style from 'styled-components';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import houses from './houses';
-import { useRequest } from './helpers';
 
 function App() {
-  const [state, setState] = useState({
-    houses,
-    hasMore: true,
-    page: 1,
-  });
-  const [resource, fetchResource] = useRequest();
-
-  const fetchData = () => {
-    const { page } = state;
-    fetchResource({ page }).then((res) => {
-      if (!res) return setState({ ...state, hasMore: false });
-      setState({ ...state, page: page + 1, hasMore: true });
-    });
-  };
-
-  useEffect(fetchData, []);
-
-  const { loading, response } = resource;
-
   return (
     <Wrapper>
       <div className="App">
-        <VisionNavBar />
-        <br />
-        <Container>
-          {!response.length ? (
-            <NoResource action={fetchData} loading={loading} />
-          ) : (
-            <InfiniteScroll
-              style={{ overflow: 'hidden' }}
-              dataLength={response.length}
-              scrollThreshold={0.9}
-              next={fetchData}
-              hasMore={state.hasMore}
-              endMessage={
-                <ErrorToast loading={loading} fetchData={fetchData} />
-              }
-              loader={<h4>Loading...</h4>}
-            >
-              <Row>
-                {response.map((home) => (
-                  <HomeCard key={home.id + Math.random()} home={home} />
-                ))}
-              </Row>
-            </InfiniteScroll>
+        <VisionNavBar>
+          {({ loading, response, hasMore, fetchData }) => (
+            <>
+              <br />
+              <Container>
+                {!response.length ? (
+                  <NoResource action={fetchData} loading={loading} />
+                ) : (
+                  <InfiniteScroll
+                    style={{ overflow: 'hidden' }}
+                    dataLength={response.length}
+                    scrollThreshold={0.9}
+                    next={fetchData}
+                    hasMore={hasMore}
+                    endMessage={
+                      <ErrorToast loading={loading} fetchData={fetchData} />
+                    }
+                    loader={<h4>Loading...</h4>}
+                  >
+                    <Row>
+                      {response.map((home) => (
+                        <HomeCard key={home.id + Math.random()} home={home} />
+                      ))}
+                    </Row>
+                  </InfiniteScroll>
+                )}
+              </Container>
+            </>
           )}
-        </Container>
+        </VisionNavBar>
       </div>
     </Wrapper>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navbar, Form, FormControl } from 'react-bootstrap';
 import { logo } from '../images';
+import { useRequest } from '../helpers';
 
-const NavBar = () => {
+const NavBar = ({ children }) => {
+  // console.log('response.loaind', state.newResources);
+
+  // create new resources
+  // use main response to search
+  // return reduced response
+
+  const [resources, fetchResource] = useRequest();
+
+  const [state, setState] = useState({
+    hasMore: true,
+    page: 1,
+    searchKey: '',
+    ...resources,
+  });
+  const fetchData = () => {
+    const { page } = state;
+    fetchResource({ page }).then((res) => {
+      if (!res) return setState({ ...state, hasMore: false });
+      setState({ ...state, page: page + 1, hasMore: true });
+    });
+  };
+
+  useEffect(fetchData, []);
+
+  const handleChange = ({ target }) => {
+    const text = target.value.toLowerCase();
+    // console.log('resource response>>>', resources.response);
+
+    const response = state.response.filter(
+      (value) =>
+        value.address.toLowerCase().includes(text) ||
+        value.homeowner.toLowerCase().includes(text)
+    );
+    // console.log('filtered response>>>', response);
+
+    // resources.response = response;
+    setState({
+      ...state,
+      searchKey: text.toLowerCase(),
+      response,
+    });
+  };
+
   return (
-    <Navbar bg="dark" variant="dark" className="justify-content-between">
-      <Navbar.Brand href="#home">
-        <img src={logo} alt="logo" className="app-logo" /> HomeVision
-      </Navbar.Brand>
-      <Form inline>
-        <FormControl type="text" placeholder="Search homes" className="mr-sm-2" />
-      </Form>
-    </Navbar>
+    <>
+      <Navbar
+        bg="dark"
+        sticky="top"
+        variant="dark"
+        className="justify-content-between"
+      >
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="logo" className="app-logo" /> HomeVision
+        </Navbar.Brand>
+        <Form inline>
+          <FormControl
+            type="text"
+            value={state.searchKey}
+            onChange={handleChange}
+            placeholder="Search homes"
+            className="mr-sm-2"
+          />
+        </Form>
+      </Navbar>
+      {children({ ...state, fetchData })}
+    </>
   );
 };
 
